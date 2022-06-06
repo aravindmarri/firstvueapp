f7de7a0cd8270e396777abf13b63cec8a9d42486
Aravind <aravindmarri99@gmail.com>
2022-06-06 18:07:05 +0530

    Used Built-in Directives

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,65 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <span v-text=" 'v-text :' +msg"></span>
+    <div v-html=" 'v-html :' + htmlTemp"></div>
+    <button @click="toggleBool()" type="button">Click here...</button>
+    <p v-once>initial value : {{ testBool }}</p>
+    <p  v-cloak>{{ testBool }}</p>
+    <div v-show="testBool">Hello Vue Show</div>
+
+    <div v-if="false">hello v-if</div>
+    <p v-else>hello v-else</p>
+    <p>v-for loop</p>
+    <ul v-for="(item, index) in emp" :key="item.id">
+      <li :id="index"> {{ item.name }} - {{ item.role }}</li>
+    </ul>
+    <div .="objectOfAttrs"> Today Date : {{ getDate() }}</div>
+
+    <input v-model="message" placeholder="edit me"/>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      v-model binding : {{ message }}
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
   props: {
     msg: String
+  },
+  setup() {
+    const objectOfAttrs = {
+      id: 'container',
+      class: 'wrapper'
+    }
+    let testBool = ref(true)
+    let inputText = "Enter Something"
+    let message = ref("Enter Something")
+
+    let emp = [
+      {name: 'Malhar', role: 'Lead'},
+      {name: 'Aravind', role: 'Web dev'},
+      {name: 'Charan', role: 'Intern'}
+    ]
+    const htmlTemp = '<p>html content</p>'
+
+    function getDate() {
+      return new Date().toString();
+    }
+
+    function toggleBool() {
+      testBool.value = !testBool.value;
+    }
+
+    return {toggleBool, getDate, htmlTemp, testBool, objectOfAttrs, emp, inputText, message}
   }
-}
+})
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -44,15 +67,22 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+[v-cloak] {
+  display: none;
 }
 </style>
